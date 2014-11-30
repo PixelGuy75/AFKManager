6ef797a2ae0b4116ae2fbb33cc20fb7f5cad050c
PixelGuy75 <crafterguy75@gmail.com>
2014-11-30 10:03:29 -0600

    Main

---
+++ src/pixelguy75/AFKplugin/Main.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AFKplugin;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener{
