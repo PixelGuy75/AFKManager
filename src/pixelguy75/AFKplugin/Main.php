7b3f7f0b981d032603631efc20fc66c59ef86b20
PixelGuy75 <crafterguy75@gmail.com>
2014-12-08 18:34:13 -0600

    Fix Main

--- src/pixelguy75/AFKplugin/Main.php
+++ src/pixelguy75/AFKplugin/Main.php
@@ -20,24 +20,24 @@ class Main extends PluginBase implements Listener{
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
 		switch($command->getName()){
 			case "afk":
-                            $player = $this->getServer()->getPlayer($sender->getName());
-                            if ($player->hasPermission("afkplugin.afk")){
-				$this->getServer()->broadcastMessage(TextFormat::GRAY . $sender->getPlayer()->getDisplayName() . " is now AFK");
-				return true;
-                            }else{
-                                $sender->sendMessage("Please run in-game!");
-                            }
-                            break;
+                $player = $this->getServer()->getPlayer($sender->getName());
+                    if ($player->hasPermission("afkplugin.afk")){
+						$this->getServer()->broadcastMessage(TextFormat::GRAY . $sender->getPlayer()->getDisplayName() . " is now AFK");
+						return true;
+                    }else{
+                        $sender->sendMessage("Please run in-game!");
+                    }
+                    break;
                             
-                        case "afkoff":
-                            $player = $this->getServer()->getPlayer($sender->getName());
-                            if ($player->hasPermission("afkplugin.afkoff")){
-				$this->getServer()->broadcastMessage(TextFormat::GRAY . $sender->getPlayer()->getDisplayName() . " is no longer AFK");
-				return true;
-                            }else{
-                                $sender->sendMessage("Please run in-game!");                              
-                            }
-                            break;
+            case "afkoff":
+                $player = $this->getServer()->getPlayer($sender->getName());
+                    if ($player->hasPermission("afkplugin.afkoff")){
+						$this->getServer()->broadcastMessage(TextFormat::GRAY . $sender->getPlayer()->getDisplayName() . " is no longer AFK");
+						return true;
+                    }else{
+                        $sender->sendMessage("Please run in-game!");                              
+                    }
+                    break;
                 }    
 		}
 }
