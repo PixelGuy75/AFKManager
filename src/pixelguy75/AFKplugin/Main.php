06ab43a1ec7d4c3f0526b2623cbbe4d9a2dba7c6
PixelGuy75 <crafterguy75@gmail.com>
2014-12-08 18:17:44 -0600

    Added color text and fixed console bug.

--- src/pixelguy75/AFKplugin/Main.php
+++ src/pixelguy75/AFKplugin/Main.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace pixelguy75\AFKplugin;
-
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\event\Listener;
@@ -9,7 +7,6 @@ use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat;
-
 class Main extends PluginBase implements Listener{
     
     public function onEnable(){
@@ -23,20 +20,24 @@ class Main extends PluginBase implements Listener{
     public function onCommand(CommandSender $sender, Command $command, $label, array $args){
 		switch($command->getName()){
 			case "afk":
-                            $player = $this->getServer()->getPlayer($sender->getName());
-                            if ($player->hasPermission("afkplugin.afk")){
-				$this->getServer()->broadcastMessage($sender->getPlayer()->getDisplayName() . " is now AFK");
-				return true;
-                            }
-                            break;
+                $player = $this->getServer()->getPlayer($sender->getName());
+                    if ($player->hasPermission("afkplugin.afk")){
+						$this->getServer()->broadcastMessage(TextFormat::GRAY . $sender->getPlayer()->getDisplayName() . " is now AFK");
+						return true;
+                    }else{
+                        $sender->sendMessage("Please run in-game!");
+                    }
+                    break;
                             
-                        case "afkoff":
-                            $player = $this->getServer()->getPlayer($sender->getName());
-                            if ($player->hasPermission("afkplugin.afkoff")){
-				$this->getServer()->broadcastMessage($sender->getPlayer()->getDisplayName() . " is no longer AFK");
-				return true;
-                            }
-                            break;
+            case "afkoff":
+                $player = $this->getServer()->getPlayer($sender->getName());
+                    if ($player->hasPermission("afkplugin.afkoff")){
+						$this->getServer()->broadcastMessage(TextFormat::GRAY . $sender->getPlayer()->getDisplayName() . " is no longer AFK");
+						return true;
+                    }else{
+                        $sender->sendMessage("Please run in-game!");                              
+                    }
+                    break;
                 }    
 		}
 }
